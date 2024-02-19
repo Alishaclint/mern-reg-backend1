e6e63dccd757e6e1a894a9a02b796576b637d7d8
Alishaclint <annaalishadarly@gmail.com>
2024-02-19 10:19:15 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,73 @@
+require('dotenv').config();
+const express = require("express");
+const PORT = process.env.PORT;
+const cors = require("cors");
+const mongoose = require("mongoose");
+const { MONGODB_URI } = process.env
+const DB = process.env.DB;
+const users = require("./userSchema");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => {
+    console.log("MongoDB connected Successfully!!!");
+  })
+  .catch((err) =>{
+     console.log('MongoDb connection error:', err)
+  });
+
+app.get("/users", async (req, res) => {
+  try {
+    const allUsers = await users.find();
+    res.status(200).json(allUsers);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+app.post("/users/register", async (req, res) => {
+  const {
+    firstName,
+    lastName,
+    email,
+    address,
+    mobile,
+    password,
+    dateOfBirth,
+    course,
+    gender,
+  } = req.body;
+
+  try {
+    const existingUser = await users.findOne({ email });
+
+    if (existingUser) return res.status(404).json("Email already exist");
+
+    const user = new users({
+      firstName,
+      lastName,
+      email,
+      address,
+      mobile,
+      password,
+      dateOfBirth,
+      course,
+      gender,
+    });
+
+    await user.save();
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log("Server is running at port ", PORT);
+});
